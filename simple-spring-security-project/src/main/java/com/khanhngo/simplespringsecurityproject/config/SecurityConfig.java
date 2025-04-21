9dd998190ef339910c8f4f9d54e63eb55130e9f4
khanhng <khanhng@vnext.vn>
2025-04-21 10:38:15 +0700

    config authentication provider, create prod profile

--- simple-spring-security-project/src/main/java/com/khanhngo/simplespringsecurityproject/config/SecurityConfig.java
+++ simple-spring-security-project/src/main/java/com/khanhngo/simplespringsecurityproject/config/SecurityConfig.java
@@ -2,16 +2,13 @@ package com.khanhngo.simplespringsecurityproject.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
